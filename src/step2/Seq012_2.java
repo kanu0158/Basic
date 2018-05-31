bb8409c2976b1d2589115c16f07dc79d3c9d579e
kanu0158 <kanu0158@naver.com>
2018-05-31 18:17:02 +0900

    step2 commit

--- src/step2/Seq012_2.java
+++ src/step2/Seq012_2.java
@@ -5,17 +5,12 @@ package step2;
 import java.util.Scanner;
 
 public class Seq012_2 {
-	public static String func(Scanner s, int t) {
+	public static String func(int label,int num1,int num2) {
 		int flag = 0;
-		switch(t) {
+		switch(label) {
 			case 1:flag=1;break;
 			case 2:flag=0;break;
 		}
-		System.out.println("num1?");
-		int num1 = s.nextInt();
-		System.out.println("num2?");
-		int num2 = s.nextInt();
-
 		if (num1 > num2) {
 			int temp = num1;
 			num1 = num2;
@@ -28,7 +23,6 @@ public class Seq012_2 {
 				totalCount++;
 			}
 		}
-
 		for (int i = start; i <= end; i++) {
 			if (i % 2 == flag) {
 				count++;
@@ -40,22 +34,28 @@ public class Seq012_2 {
 				sum += i;
 			}
 		}
-		
 		return result+sum;
 	}
 
 	public static void main(String[] args) {
-		
 		while (true) {
 			Scanner s = new Scanner(System.in);
 			System.out.println("0.STOP 1.ODD 2.EVEN");
 			switch (s.nextInt()) {
 			case 0:return;
 			case 1:
-				System.out.println(func(s,1));
+				System.out.println("num1?");
+				int num1 = s.nextInt();
+				System.out.println("num2?");
+				int num2 = s.nextInt();
+				System.out.println(func(1,num1,num2));
 				break;
 			case 2:
-				System.out.println(func(s,2));
+				System.out.println("num1?");
+				num1 = s.nextInt();
+				System.out.println("num2?");
+				num2 = s.nextInt();
+				System.out.println(func(2,num1,num2));
 				break;
 			default:
 				return;
