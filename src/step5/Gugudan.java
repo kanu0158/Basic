9a6b02a4f1def8b48d8c3262f7e7dcf523432639
kanu0158 <kanu0158@naver.com>
2018-06-07 18:04:47 +0900

    commit

---
+++ src/step5/Gugudan.java
@@ -0,0 +1,54 @@
+package step5;
+
+import javax.swing.JOptionPane;
+enum GugudanButt{
+	EXIT,
+	OUTPUT
+}
+public class Gugudan {
+	public static void main(String[] args) {
+		String[][] mtx = new String[9][9];
+		for(int i=0;i<8;i++) {
+			for(int j=0;j<9;j++) {
+				mtx[i][j] = String.valueOf((i+2) * (j+1));
+			}
+		}
+		while(true) {
+			GugudanButt[] buttons = {
+					GugudanButt.EXIT,
+					GugudanButt.OUTPUT
+			};
+			GugudanButt select = (GugudanButt) JOptionPane.showInputDialog(null,//frame
+					"구구단",//frame title
+					"기능을 선택하시오",//order
+					JOptionPane.QUESTION_MESSAGE,//type
+					null,//icon
+					buttons,//Array of choices
+					buttons[1]//default
+					);
+			switch(select) {
+			case EXIT:return;
+			case OUTPUT:
+				String[] result1 = new String[9];
+				String[] result2 = new String[9];
+				String result3 = "";
+				String result4 = "\n";
+				for(int i=0;i<9;i++) {
+					result1[i] = "";
+					result2[i] = "";
+					for(int j=0;j<4;j++) {
+						result1[i] += (j+2) + " x " + (i+1) + " = " + mtx[j][i] + "   ";
+						result2[i] += (j+6) + " x " + (i+1) + " = " + mtx[j+4][i] + "   "; 
+					}
+					result1[i] += "\n";
+					result3 += result1[i];
+					result2[i] += "\n";
+					result4 += result2[i];
+				}
+				result4 = result3 + result4;
+				JOptionPane.showMessageDialog(null, result4);
+				break;
+			}
+		}
+	}
+}
